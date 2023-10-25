bbeca70a1a1cd607853e4445bfe1ba6a84c7f218
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-10-25 10:51:00 -0400

    Activity 8.2

--- sorts.py
+++ sorts.py
@@ -3,8 +3,17 @@ import random
 def random_list(length):
     return [random.randint(0,length) for x in range(length)]
 
+def swap(a_list, a, b):
+    temp_val = a_list[a]
+    a_list[a] = a_list[b]
+    a_list[b] = temp_val
+    return a_list
+
 def main():
-    print(random_list(100))
+    # print(random_list(100))
+    some_list = [x for x in range(10)]
+    print(swap(some_list, 2, 3))
+
 
 if __name__ == "__main__":
     main()
