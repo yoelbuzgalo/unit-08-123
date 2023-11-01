959a2efce7e9425e0d1d0815d738fd1f1bdab583
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-01 00:25:56 -0400

    Assignment 8.2 (Part 3)

--- idiots_delight.py
+++ idiots_delight.py
@@ -7,3 +7,20 @@ def deal_hands():
     deck = cards.make_deck() # Create a deck
     hand = cards.deal_one_hand(deck, 4) # Draw 4 cards accordingly from the deck
     return deck, hand
+
+def discard(a_hand, num_cards):
+    '''
+    This function checks and discards cards given the num_card parameter
+    '''
+    
+    if (num_cards != 4 and num_cards != 2) or (len(a_hand) < 4):
+        return a_hand
+    
+    if num_cards == 4:
+        for _ in range(4):
+            a_hand.pop()
+    elif num_cards == 2:
+        a_hand.pop(-2)
+        a_hand.pop(-2)
+    
+    return a_hand
--- idiots_delight_test.py
+++ idiots_delight_test.py
@@ -14,3 +14,47 @@ def test_deal_hands():
     # Analysis
     assert len(result_deck) == expected_deck_len
     assert result_hand == expected_hand
+
+def test_discard_4():
+    # Setup
+    hand = [(14, 'Spades', 'Ace of Spades', '\x1b[34mAS\x1b[37m'),(14, 'Hearts', 'Ace of Hearts', '\x1b[31mAH\x1b[37m'),(14, 'Diamonds', 'Ace of Diamonds', '\x1b[31mAD\x1b[37m'),(14, 'Clubs', 'Ace of Clubs', '\x1b[34mAC\x1b[37m')]
+    expected = []
+
+    # Invoke
+    result = idiots_delight.discard(hand, 4)
+
+    # Analysis
+    assert result == expected
+
+def test_discard_2():
+    # Setup
+    hand = [(14, 'Spades', 'Ace of Spades', '\x1b[34mAS\x1b[37m'),(14, 'Hearts', 'Ace of Hearts', '\x1b[31mAH\x1b[37m'),(14, 'Diamonds', 'Ace of Diamonds', '\x1b[31mAD\x1b[37m'),(14, 'Clubs', 'Ace of Clubs', '\x1b[34mAC\x1b[37m')]
+    expected = [(14, 'Spades', 'Ace of Spades', '\x1b[34mAS\x1b[37m'),(14, 'Clubs', 'Ace of Clubs', '\x1b[34mAC\x1b[37m')]
+
+    # Invoke
+    result = idiots_delight.discard(hand, 2)
+
+    # Analysis
+    assert result == expected
+
+def test_discard_3():
+    # Setup
+    hand = [(14, 'Spades', 'Ace of Spades', '\x1b[34mAS\x1b[37m'),(14, 'Hearts', 'Ace of Hearts', '\x1b[31mAH\x1b[37m'),(14, 'Diamonds', 'Ace of Diamonds', '\x1b[31mAD\x1b[37m'),(14, 'Clubs', 'Ace of Clubs', '\x1b[34mAC\x1b[37m')]
+    expected = [(14, 'Spades', 'Ace of Spades', '\x1b[34mAS\x1b[37m'),(14, 'Hearts', 'Ace of Hearts', '\x1b[31mAH\x1b[37m'),(14, 'Diamonds', 'Ace of Diamonds', '\x1b[31mAD\x1b[37m'),(14, 'Clubs', 'Ace of Clubs', '\x1b[34mAC\x1b[37m')]
+
+    # Invoke
+    result = idiots_delight.discard(hand, 3)
+
+    # Analysis
+    assert result == expected
+
+def test_discard_empty():
+    # Setup
+    hand = []
+    expected = []
+
+    # Invoke
+    result = idiots_delight.discard(hand, 4)
+
+    # Analysis
+    assert result == expected
